389bdda8ab6c7c6b6cefa8cd12b1ba83ffff2fcc
Yohan Etifier <yohan@adelios.fr>
2022-07-03 15:01:20 +0200

    add title to the section our services

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -1,21 +1,22 @@
-import styles from './Service.module.scss'; 
-import img1 from '../../assets/images/hayley-kim-design-sRSRuxkOuzI-unsplash.jpg'; 
-import img2 from '../../assets/images/gabe-pierce-_wK8VnF5yao-unsplash.jpg'; 
+import styles from './Service.module.scss';
+import img1 from '../../assets/images/hayley-kim-design-sRSRuxkOuzI-unsplash.jpg';
+import img2 from '../../assets/images/gabe-pierce-_wK8VnF5yao-unsplash.jpg';
 // import img3 from '../../assets/images/kartik-gada--4iMX-4MIZ8-unsplash.jpg'; 
-import img3 from '../../assets/images/giorgio-trovato-gb6gtiTZKB8-unsplash.jpg'; 
+import img3 from '../../assets/images/giorgio-trovato-gb6gtiTZKB8-unsplash.jpg';
 
 function Service() {
     return (
         <div className={styles.mainwrapper}>
+            <h2>Nos Services</h2>
             <div className={styles.imgwrapper}>
                 <div className={styles.img1wrapper}>
                     <img src={img1} alt="" className={styles.img1} />
                 </div>
                 <div className={styles.img2wrapper}>
-                    <img src={img2} alt="" className={styles.img2}/>
+                    <img src={img2} alt="" className={styles.img2} />
                 </div>
                 <div className={styles.img3wrapper}>
-                    <img src={img3} alt=""  className={styles.img3}/>
+                    <img src={img3} alt="" className={styles.img3} />
                 </div>
             </div>
             <div>
