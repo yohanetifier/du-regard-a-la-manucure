5e59ac8d2339dcfe8a8234a6559fd4b750529b67
yohanetifier <etifier.y@gmail.com>
2022-11-19 13:06:00 +0100

    finish translate the img by the left with react transition group

--- src/components/Concept/Concept.tsx
+++ src/components/Concept/Concept.tsx
@@ -1,15 +1,15 @@
 import Image from '../Image/Image';
 import styles from './Concept.module.scss';
 import Button from "../Button/Button";
-import { useState, useLayoutEffect, useRef } from "react";
-import { useSpring, animated } from '@react-spring/web';
-import useIntersection from '../UseIntersection/UseIntersection';
+import { CSSTransition } from "react-transition-group";
+import useIntersection from "../UseIntersection/UseIntersection";
+import { useRef, useState } from "react";
 
 export interface Img {
     src: string
     alt: string
     classNamewrapper?: string
-    ref?: null
+    ref?: HTMLElement
 }
 
 interface Props {
@@ -37,57 +37,77 @@ function Concept({
     numberOfButton,
 
 }: Props) {
+
     const wrapper = useRef<null>(null);
-    const wrapperDescription = useRef<null>(null);
+    const [state, setState] = useState(false);
+    // const wrapperDescription = useRef<null>(null);
     const ifIsInViewport = useIntersection(wrapper, 0.5);
 
-    const translationByTheLeft = useSpring({
-        from: { transform: "translateX(-100vw)" },
-        to: { transform: ifIsInViewport ? "translateX(0px)" : "translateX(-100vw)" },
-        config: { duration: 1000 }
-    })
+    function changeValue() {
+        setTimeout(() => {
+            setState(true)
+        }, 10000)
+    }
+    changeValue();
+    // console.log("ifIsInViewport", ifIsInViewport)
+    // const translationByTheLeft = useSpring({
+    //     from: { transform: "translateX(-100vw)" },
+    //     to: { transform: ifIsInViewport ? "translateX(0px)" : "translateX(-100vw)" },
+    //     config: { duration: 1000 }
+    // })
 
-    const translationByTheRight = useSpring({
-        from: { transform: "translateX(100vw)" },
-        to: { transform: ifIsInViewport ? 'translateX(0px)' : "translateX(100vw)" },
-        config: { duration: 1000 }
-    })
+    // const translationByTheRight = useSpring({
+    //     from: { transform: "translateX(100vw)" },
+    //     to: { transform: ifIsInViewport ? 'translateX(0px)' : "translateX(100vw)" },
+    //     config: { duration: 1000 }
+    // })
 
     return (
 
-        <div className={styles.mainwrapper}>
+        <div className={styles.mainwrapper} >
             <div className={styles.imagewrapper} ref={wrapper}>
-                {ifIsInViewport && <animated.img
-                    src={src}
-                    alt={alt}
-                    className={styles.firstimage}
-                    style={{ ...translationByTheLeft }}
-                />}
+                < CSSTransition
+                    in={ifIsInViewport}
+                    timeout={1000}
+                    classNames={{
+                        enterActive: styles.testEnterActive,
+                        enterDone: styles.testEnterDone,
+                        exitActive: styles.ExitActive,
+                        exitDone: styles.testExitDone
+                    }}
+                >
+                    <img
+                        src={src}
+                        alt={alt}
+                        className={styles.test}
+                    // className="test"
+                    />
+                </CSSTransition>
+
 
 
             </div>
-            {ifIsInViewport &&
-                <animated.div className={styles.descriptionwrapper} ref={wrapperDescription} style={{ ...translationByTheRight }}>
-                    <div className={styles.layoutwrapper}>
-                        <h2 className={styles.title}> {title}</h2>
-                        <p className={styles.description}>
-                            {children}</p>
-                        {numberOfButton === "one" ?
+            <div className={styles.descriptionwrapper} >
+                <div className={styles.layoutwrapper}>
+                    <h2 className={styles.title}> {title}</h2>
+                    <p className={styles.description}>
+                        {children}</p>
+                    {numberOfButton === "one" ?
+                        <Button
+                            to={to}
+                            label={label}
+                        />
+                        :
+                        <div className={styles.buttonwrapper}>
                             <Button
                                 to={to}
-                                label={label}
-                            />
-                            :
-                            <div className={styles.buttonwrapper}>
-                                <Button
-                                    to={to}
-                                    label={label} />
-                                <Button
-                                    to={secondTo!}
-                                    label={secondLabel!} />
-                            </div>}
-                    </div>
-                </animated.div>}
+                                label={label} />
+                            <Button
+                                to={secondTo!}
+                                label={secondLabel!} />
+                        </div>}
+                </div>
+            </div>
         </div >
 
     )
