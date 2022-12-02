dcb6246cb199c23852dab7887d737df1ec9e8ea8
yohanetifier <etifier.y@gmail.com>
2022-12-02 23:25:12 +0100

    DRALM-26 animation on routes

--- src/components/AnimatedRoutes/AnimatedRoutes.tsx
+++ src/components/AnimatedRoutes/AnimatedRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Home from '../Home/Home'
 import Contact from '../Contact/Contact';
 import Service from '../Service/Service';
@@ -6,14 +6,17 @@ import test from '../../assets/images/amy-shamblen-xwM61TPMlYk-unsplash.jpg';
 import About from '../About/About';
 import Prestations from '../Prestations/Prestations';
 import Success from '../Success/Success';
+import { AnimatePresence } from "framer-motion";;
 
 interface Props {
     setLastAnimationIsOver(arg: boolean): void
 }
 
 function AnimatedRoutes({ setLastAnimationIsOver }: Props) {
+    const location = useLocation(); 
     return (
-        <Routes>
+        <AnimatePresence>
+        <Routes location={location} key={location.pathname}>
             <Route path="/" element={<Home animationDone={setLastAnimationIsOver} />} />
             <Route path="/services" element={<Service img={test} />} />
             <Route path="/contact" element={<Contact />} />
@@ -21,6 +24,7 @@ function AnimatedRoutes({ setLastAnimationIsOver }: Props) {
             <Route path="/prestations" element={<Prestations />} />
             <Route path="/succes" element={<Success />} />
         </Routes>
+        </AnimatePresence>
     )
 }
 
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -7,16 +7,44 @@ import Work from '../Work/Work';
 import Footer from '../Footer/Footer';
 import imgBelowRight from '../../assets/images/apostolos-vamvouras-Mky1mc85XQQ-unsplash (1).jpg'
 import img1 from '../../assets/images/clay-banks-_3Sud4WPPYE-unsplash.jpg';
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 export interface Props {
     animationDone(arg: boolean): void
 }
 
 function Home({animationDone}: Props) {
-
+    const [ isLoading, setIsLoading ] = useState(false); 
+    // const removeLoading = () => {
+    //     setTimeout(() => {
+    //         setIsLoading(true)
+    //     }, 100)
+    // }
     return (
-        <div className={styles.wrapper}>
-            <Banner
+        <div>
+        {!isLoading ? 
+        <motion.div 
+        className={styles.wrapper}
+        initial={{opacity: 0,}}
+        animate={{opacity: 1 }}
+        transition={{duration: 3}}
+        onAnimationComplete={() => setIsLoading(true)}
+        
+        >
+            <p>Du regard à la Manucure</p>
+             </motion.div>     
+        : 
+        
+        <motion.div 
+            // className={styles.wrapperTest}
+            initial={{x: 500}}
+            animate={{x: 0 }}
+            transition={{
+                duration: 3
+            }}
+        >
+             <Banner
                 src={imgBelowRight}
                 className={styles.imgbelowright}
                 classNamewrapper={styles.mainwrapper}
@@ -34,8 +62,30 @@ function Home({animationDone}: Props) {
             />
             <SecondSection />
             <Services />
-            <Work animationDone={animationDone}/>
+            <Work animationDone={animationDone}/> 
+             </motion.div>     }
+
+            {/* <Banner
+                src={imgBelowRight}
+                className={styles.imgbelowright}
+                classNamewrapper={styles.mainwrapper}
+            />
+            <Concept
+                src={img1}
+                alt={img1}
+                title="Notre Concept"
+                children="Que diriez-vous de sublimer vos ongles et votre regard sans sortir de chez vous? C'est possible avec Du Regard à La Manucure,
+                professionnelle de l'esthétique, prothésiste ongulaire. L'institut Du Regard à la Manucure est basé à Aincourt, dans le Val d'Oise et se
+                déplace dans toute l'Ile de France pour vous mettre en beauté."
+                to="/about"
+                label="En savoir plus"
+                numberOfButton="one"
+            />
+            <SecondSection />
+            <Services />
+            <Work animationDone={animationDone}/> */}
             {/* <Footer /> */}
+       
         </div>
     )
 }
