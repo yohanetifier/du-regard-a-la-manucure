916d7ac424326a30ccc96acee5f423277925b1d9
yohanetifier <etifier.y@gmail.com>
2022-12-08 23:46:56 +0100

    DRALM-26 animation in progress

--- src/components/About/Concept2/LayoutThreeImg/LayoutThreeImg.tsx
+++ src/components/About/Concept2/LayoutThreeImg/LayoutThreeImg.tsx
@@ -3,14 +3,14 @@ import styles from './LayoutThreeImg.module.scss';
 import { Assets } from '../Concept2';
 import { motion, useInView } from "framer-motion";
 import { useState, useRef, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 
 export default function LayoutThreeImg({ assets }: Assets) {
     const wrapper = useRef(null);
     const isInView = useInView(wrapper, { once: true, amount: 0.5 });
-    const [inView, setInView] = useState(true);
-
-
+    const [ inView, setInView ] = useState(false);
+    
     const container = {
         initial: {
             scale: 0,
@@ -19,7 +19,7 @@ export default function LayoutThreeImg({ assets }: Assets) {
             scale: 1,
             transition: {
                 delayChildren: .6,
-                staggerChildren: 0.9
+                staggerChildren: 5
             }
         }
     }
@@ -31,7 +31,7 @@ export default function LayoutThreeImg({ assets }: Assets) {
         animate: {
             scale: 1,
             transition: {
-                duration: 5
+                duration: 10
             }
         },
     }
@@ -43,18 +43,24 @@ export default function LayoutThreeImg({ assets }: Assets) {
             className={styles.imgwrapper}
             ref={wrapper}
             variants={container}
+            initial="initial"
+            animate="animate"
         >
-            {assets.map(({ src, alt, className }) => (
-                <motion.div
+            {assets.map(({ src, alt, className}) => (
+                <>
+            <motion.div
                     className={`${styles[className!]}`}
                     variants={item}
-                >
-                    <Image
+                    // key={index}
+                    >
+                    { inView && <Image
                         src={src}
                         alt={alt}
                         classNamewrapper={styles.fullHeight}
-                    />
+                        // variants={item}
+                        />}
                 </motion.div>
+                </>
 
 
             ))}
