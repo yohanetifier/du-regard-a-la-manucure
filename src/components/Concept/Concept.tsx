b3cdce797aef6e725364de24d95173cea6f71fc8
yohanetifier <etifier.y@gmail.com>
2023-01-26 20:25:19 +0100

    add prettier rc to avoid bad format in the future

--- src/components/Concept/Concept.tsx
+++ src/components/Concept/Concept.tsx
@@ -1,145 +1,151 @@
 import Image from '../Image/Image';
 import styles from './Concept.module.scss';
-import Button from "../Button/Button";
-import { useRef, } from "react";
-import { motion, useInView } from "framer-motion";
+import Button from '../Button/Button';
+import { useRef } from 'react';
+import { motion, useInView } from 'framer-motion';
 import { useMediaQuery } from 'react-responsive';
 
-
 interface AnimationProps {
-    opacity?: number
-    x?: string | number
-    duration?: number
+	opacity?: number;
+	x?: string | number;
+	duration?: number;
 }
 
 interface VariantsProps {
-    initial: AnimationProps
-    animate: AnimationProps
+	initial: AnimationProps;
+	animate: AnimationProps;
 }
 
 export interface Img {
-    src: string
-    alt: string
-    classNamewrapper?: string
-    ref?: string
+	src: string;
+	alt: string;
+	classNamewrapper?: string;
+	ref?: string;
 }
 
 interface Props {
-    src: string
-    alt: string
-    title: string
-    children: string
-    to: string
-    label: string
-    secondTo?: string
-    secondLabel?: string
-    numberOfButton: string
-
+	src: string;
+	alt: string;
+	title: string;
+	children: string;
+	to: string;
+	label: string;
+	secondTo?: string;
+	secondLabel?: string;
+	numberOfButton: string;
 }
 
 function Concept({
-    src,
-    alt,
-    title,
-    children,
-    to,
-    label,
-    secondTo,
-    secondLabel,
-    numberOfButton,
-
+	src,
+	alt,
+	title,
+	children,
+	to,
+	label,
+	secondTo,
+	secondLabel,
+	numberOfButton,
 }: Props) {
+	const scrollRef = useRef(null);
+	const isInView = useInView(scrollRef, { once: true, amount: 0.2 });
 
-    const scrollRef = useRef(null);
-    const isInView = useInView(scrollRef, { once: true, amount: 0.2 });
-
-    return (
-
-        <div className={styles.mainwrapper} ref={scrollRef}>
-            {isInView &&
-                <motion.div
-                    className={styles.imagewrapper}
-                    initial={{ x: "-100vw" }}
-                    animate={{ x: 0 }}
-                    transition={{ duration: 1 }}
-                >
-                    <Image
-                        src={src}
-                        alt={alt}
-                        classNamewrapper={styles.firstimage}
-                    />
-                </motion.div>}
-            {isInView &&
-                <motion.div
-                    className={styles.descriptionwrapper}
-                    initial={{ x: "100vw" }}
-                    animate={{ x: 0 }}
-                    transition={{ duration: 1 }}
-                >
-                    <div className={styles.layoutwrapper}>
-                        <span>
-                            <motion.h2
-                                className={styles.title}
-                                initial={{ y: 200 }}
-                                animate={{ y: 0 }}
-                                transition={{
-                                    delay: 2,
-                                    duration: 1
-                                }}
-                            > {title}
-                            </motion.h2>
-                        </span>
-                        <div className={styles.exercice}>
-                            <motion.p
-                                initial={{ y: 300 }}
-                                animate={{ y: 0 }}
-                                transition={{
-                                    delay: 3,
-                                    duration: 1,
-                                    staggerChildren: 0.05
-                                }}
-                            >
-                                {children}
-                            </motion.p>
-                        </div>
-                        {numberOfButton === "one" ?
-                            <motion.div
-                                className={styles.buttonwrapper}
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                                transition={{
-                                    delay: 4,
-                                    duration: 1,
-                                }}
-                            >
-                                <Button
-                                    to={to}
-                                    label={label}
-                                    width="200px"
-                                />
-                            </motion.div>
-                            :
-                            <motion.div
-                                className={styles.buttonwrapper}
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                                transition={{
-                                    delay: 4,
-                                    duration: 1,
-                                }}
-                            >
-                                <Button
-                                    to={to}
-                                    label={label} />
-                                <Button
-                                    to={secondTo!}
-                                    label={secondLabel!} />
-                            </motion.div>}
-                    </div>
-                </motion.div>}
-        </div >
-
-    )
+	return (
+		<div className={styles.mainwrapper} ref={scrollRef}>
+			{isInView && (
+				<motion.div
+					className={styles.imagewrapper}
+					initial={{ x: '-100vw' }}
+					animate={{ x: 0 }}
+					transition={{ duration: 1 }}
+				>
+					<Image
+						src={src}
+						alt={alt}
+						classNamewrapper={styles.firstimage}
+					/>
+				</motion.div>
+			)}
+			{isInView && (
+				<motion.div
+					className={styles.descriptionwrapper}
+					initial={{ x: '100vw' }}
+					animate={{ x: 0 }}
+					transition={{ duration: 1 }}
+				>
+					<div className={styles.layoutwrapper}>
+						<span>
+							<motion.h2
+								className={styles.title}
+								initial={{
+									y: 200,
+								}}
+								animate={{
+									y: 0,
+								}}
+								transition={{
+									delay: 2,
+									duration: 1,
+								}}
+							>
+								{title}
+							</motion.h2>
+						</span>
+						<div className={styles.exercice}>
+							<motion.p
+								initial={{
+									y: 300,
+								}}
+								animate={{
+									y: 0,
+								}}
+								transition={{
+									delay: 3,
+									duration: 1,
+									staggerChildren: 0.05,
+								}}
+							>
+								{children}
+							</motion.p>
+						</div>
+						{numberOfButton === 'one' ? (
+							<motion.div
+								className={styles.buttonwrapper}
+								initial={{
+									opacity: 0,
+								}}
+								animate={{
+									opacity: 1,
+								}}
+								transition={{
+									delay: 4,
+									duration: 1,
+								}}
+							>
+								<Button to={to} label={label} width="200px" />
+							</motion.div>
+						) : (
+							<motion.div
+								className={styles.buttonwrapper}
+								initial={{
+									opacity: 0,
+								}}
+								animate={{
+									opacity: 1,
+								}}
+								transition={{
+									delay: 4,
+									duration: 1,
+								}}
+							>
+								<Button to={to} label={label} />
+								<Button to={secondTo!} label={secondLabel!} />
+							</motion.div>
+						)}
+					</div>
+				</motion.div>
+			)}
+		</div>
+	);
 }
 
-export default Concept; 
+export default Concept;
