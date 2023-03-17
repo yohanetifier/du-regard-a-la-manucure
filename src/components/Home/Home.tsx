3f40c8e6d69aba120185bdbd3d5f33041c398f5d
yohanetifier <etifier.y@gmail.com>
2023-03-17 21:17:27 +0100

    add banner to prestations with text

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,22 +1,22 @@
-import Banner from '../Banner/Banner';
-import styles from './Home.module.scss';
-import SecondSection from '../SecondSection/SecondSection';
-import Concept from '../Concept/Concept';
-import Services from '../Services/Services';
-import Work from '../Work/Work';
-import Footer from '../Footer/Footer';
-import imgBelowRight from '../../assets/images/apostolos-vamvouras-Mky1mc85XQQ-unsplash-_1_.webp';
-import img1 from '../../assets/images/clay-banks-_3Sud4WPPYE-unsplash.webp';
-import img from '../../assets/images/henry-co-OpjlRo-31SI-unsplash.jpg';
-import img2 from '../../assets/images/oliver-johnson-yH0dth2yEQE-unsplash.jpg';
-import img3 from '../../assets/images/kartik-gada--4iMX-4MIZ8-unsplash.jpg';
-import img4 from '../../assets/images/majid-akbari--CE-5hQSoOQ-unsplash.jpg';
-import { motion } from 'framer-motion';
-import { useState } from 'react';
-import { Img } from '../Concept/Concept';
-import PageTransition from '../PageTransition/PageTransition';
-import imgSecondSection1 from '../../assets/images/sara-dabaghian-wZx6BeqZNUk-unsplash.webp';
-import imgSecondSection2 from '../../assets/images/hadis-safari-A7rkoSFjrG0-unsplash.webp';
+import Banner from "../Banner/Banner";
+import styles from "./Home.module.scss";
+import SecondSection from "../SecondSection/SecondSection";
+import Concept from "../Concept/Concept";
+import Services from "../Services/Services";
+import Work from "../Work/Work";
+import Footer from "../Footer/Footer";
+import imgBelowRight from "../../assets/images/apostolos-vamvouras-Mky1mc85XQQ-unsplash-_1_.webp";
+import img1 from "../../assets/images/clay-banks-_3Sud4WPPYE-unsplash.webp";
+import img from "../../assets/images/henry-co-OpjlRo-31SI-unsplash.jpg";
+import img2 from "../../assets/images/oliver-johnson-yH0dth2yEQE-unsplash.jpg";
+import img3 from "../../assets/images/kartik-gada--4iMX-4MIZ8-unsplash.jpg";
+import img4 from "../../assets/images/majid-akbari--CE-5hQSoOQ-unsplash.jpg";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { Img } from "../Concept/Concept";
+import PageTransition from "../PageTransition/PageTransition";
+import imgSecondSection1 from "../../assets/images/sara-dabaghian-wZx6BeqZNUk-unsplash.webp";
+import imgSecondSection2 from "../../assets/images/hadis-safari-A7rkoSFjrG0-unsplash.webp";
 
 function Home() {
 	const workImage: Img[] = [
@@ -40,19 +40,19 @@ function Home() {
 
 	const quote = [
 		{
-			sentence: 'La beauté',
+			sentence: "La beauté",
 		},
 		{
-			sentence: 'est',
+			sentence: "est",
 		},
 		{
-			sentence: 'dans les yeux',
+			sentence: "dans les yeux",
 		},
 		{
-			sentence: 'de celui qui regarde',
+			sentence: "de celui qui regarde",
 		},
 		{
-			sentence: 'Oscar Wilde',
+			sentence: "Oscar Wilde",
 		},
 	];
 
@@ -65,16 +65,22 @@ function Home() {
 					animate={{ opacity: 1, transition: { duration: 2 } }}
 					exit={{ opacity: 0, transition: { duration: 1 } }}
 				>
-					<Banner src={imgBelowRight} />
+					<Banner
+						src={imgBelowRight}
+						title="Plus qu'une parenthèse bien être... une mise en beauté unique"
+						hasButton={true}
+						linkButton="https://www.planity.com/du-regard-a-la-manucure-deplacement-a-domicile-95510-aincourt"
+						labelButton="Reservez"
+					/>
 					<Concept
 						src={img1}
 						alt={img1}
-						title='Notre Concept'
+						title="Notre Concept"
 						children="Que diriez-vous de sublimer vos ongles et votre regard sans sortir de chez vous? C'est possible avec Du Regard à La Manucure,
                 professionnelle de l'esthétique, prothésiste ongulaire. L'institut Du Regard à la Manucure est basé à Aincourt, dans le Val d'Oise et se
                 déplace dans toute l'Ile de France pour vous mettre en beauté."
-						to='/about'
-						label='En savoir plus'
+						to="/about"
+						label="En savoir plus"
 						// numberOfButton="one"
 					/>
 					<SecondSection
@@ -85,7 +91,7 @@ function Home() {
 						quote={quote}
 					/>
 					<Services />
-					<Work title='Nos Succès' img={workImage} />
+					<Work title="Nos Succès" img={workImage} />
 				</motion.div>
 				<Footer />
 			</PageTransition>
--- src/components/Service/Service.tsx
+++ src/components/Service/Service.tsx
@@ -1,118 +1,197 @@
-import { useContext, useState } from 'react';
-import Banner from '../Banner/Banner';
-import Image from '../Image/Image';
-import styles from './Service.module.scss';
-import ServiceBar from './ServiceBar/ServiceBar';
-import imgTopNails from '../../assets/images/jesse-donoghoe-2aiP_wxNrfU-unsplash.jpg';
-import defaultImgBottom from '../../assets/images/sour-moha-k47viB7Dt8I-unsplash.jpg';
-import defaultImgTop from '../../assets/images/billie-5OXE3KjDEfI-unsplash.jpg';
-import imgBottomNails from '../../assets/images/giorgio-trovato-gb6gtiTZKB8-unsplash.jpg';
-import imgTopEyebrows from '../../assets/images/amanda-dalbjorn-fvInY-Gh7sc-unsplash.jpg';
-import imgBottomEyebrows from '../../assets/images/gabe-pierce-_wK8VnF5yao-unsplash.jpg';
-import imgTopEyelashes from '../../assets/images/PHOTO-2022-12-31-13-01-03.jpg';
-import imgBottomEyelashes from '../../assets/images/hayley-kim-design-sRSRuxkOuzI-unsplash.jpg';
-import { beautyOfHands } from './ServiceCard/BeautyOfNails/BeautyOfHands';
-import { beautyOfFoot } from './ServiceCard/BeautyOfNails/BeautyOfFoot';
-import { beautyOfEyebrows } from './ServiceCard/beautyOfEyebrows';
-import { beautyOfEyelashes } from './ServiceCard/beautyOfEyelashes';
-import PageTransition from '../PageTransition/PageTransition';
-import { SelectedService } from '../App/App';
+import { useContext, useState } from "react";
+import Banner from "../Banner/Banner";
+import Image from "../Image/Image";
+import styles from "./Service.module.scss";
+import ServiceBar from "./ServiceBar/ServiceBar";
+import imgTopNails from "../../assets/images/jesse-donoghoe-2aiP_wxNrfU-unsplash.jpg";
+import defaultImgBottom from "../../assets/images/sour-moha-k47viB7Dt8I-unsplash.jpg";
+import defaultImgTop from "../../assets/images/billie-5OXE3KjDEfI-unsplash.jpg";
+import imgBottomNails from "../../assets/images/giorgio-trovato-gb6gtiTZKB8-unsplash.jpg";
+import imgTopEyebrows from "../../assets/images/amanda-dalbjorn-fvInY-Gh7sc-unsplash.jpg";
+import imgBottomEyebrows from "../../assets/images/gabe-pierce-_wK8VnF5yao-unsplash.jpg";
+import imgTopEyelashes from "../../assets/images/PHOTO-2022-12-31-13-01-03.jpg";
+import imgBottomEyelashes from "../../assets/images/hayley-kim-design-sRSRuxkOuzI-unsplash.jpg";
+import { beautyOfHands } from "./ServiceCard/BeautyOfNails/BeautyOfHands";
+import { beautyOfFoot } from "./ServiceCard/BeautyOfNails/BeautyOfFoot";
+import { beautyOfEyebrows } from "./ServiceCard/beautyOfEyebrows";
+import { beautyOfEyelashes } from "./ServiceCard/beautyOfEyelashes";
+import PageTransition from "../PageTransition/PageTransition";
+import { SelectedService } from "../App/App";
 
 interface Props {
-    img: string
+	img: string;
 }
 
 export default function Service({ img }: Props) {
-    const choosenService = useContext(SelectedService);
-    // const [changeService, setChangeService] = useState<string>('');
-    const [image, setImage] = useState<string>('');
-    const { selectedService, setSelectedService } = choosenService;
-    console.log('selectedService', selectedService);
-    console.log('setSelectedService', setSelectedService);
+	const choosenService = useContext(SelectedService);
+	// const [changeService, setChangeService] = useState<string>('');
+	const [image, setImage] = useState<string>("");
+	const { selectedService, setSelectedService } = choosenService;
+	console.log("selectedService", selectedService);
+	console.log("setSelectedService", setSelectedService);
 
-
-
-    return (
-        <PageTransition>
-            <Banner src={img} className={styles.img} classNamewrapper={styles.wrapper} />
-            <ServiceBar
-                toggleService={selectedService}
-                changeClass={(selectedService: string) => setSelectedService(selectedService)}
-            />
-            {!selectedService ? (
-                <div className={styles.wrappercenter}>
-                    <div className={styles.wrapperimgbottom}>
-                        <img src={defaultImgBottom} alt="" className={styles.img} />
-                    </div>
-                    <h1 className={styles.layoutitle}>De la beauté du regard</h1>
-                    <div className={styles.wrapperimgtop}>
-                        <img src={defaultImgTop} alt="" className={styles.img} />
-                    </div>
-                </div>
-            ) : (
-                <div className={styles.wrappercenter}>
-                    <div className={styles.price}>
-                        <div className={selectedService === 'Ongles' ? `${styles.ongles} ${styles.actif}` : `${styles.ongles}`}>
-                            <h2 className={styles.subtitle}>Beauté des mains</h2>
-                            <ul>
-                                {beautyOfHands.map(({ service, price }) => (
-                                    <div className={styles.beautywrapper}>
-                                        <li>{service}</li>
-                                        <span className={styles.bold}>{price}</span>
-                                    </div>
-
-                                ))}
-                            </ul>
-                            <div className={styles.test}>
-                                <h2 className={styles.subtitle}>Beauté des pieds</h2>
-                                <ul>
-                                    {beautyOfFoot.map(({ service, price }) => (
-                                        <div className={styles.beautywrapper}>
-                                            <li>{service}</li>
-                                            <span className={styles.bold}>{price}</span>
-                                        </div>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
-                        <div className={selectedService === 'Sourcils' ? `${styles.sourcils} ${styles.actif}` : `${styles.sourcils}`}>
-                            <h2 className={styles.subtitle}>Beauté du regard</h2>
-                            <ul>
-                                {beautyOfEyebrows.map(({ service, price }) => (
-                                    <div className={styles.beautywrapper}>
-                                        <li>{service}</li>
-                                        <span className={styles.bold}>{price}</span>
-                                    </div>
-                                ))}
-                            </ul>
-                        </div>
-                        <div className={selectedService === 'Cils' ? `${styles.cils} ${styles.actif}` : `${styles.cils}`}>
-                            <h2 className={styles.subtitle}>Beauté du regard</h2>
-                            <ul className={styles.listofservice}>
-                                {beautyOfEyelashes.map(({ service, price }) => (
-                                    <div className={styles.beautywrapper}>
-                                        <li>{service}</li>
-                                        <span className={styles.bold}>{price}</span>
-                                    </div>
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
-                    <div className={selectedService === 'Ongles' ? `${styles.foot} ${styles.actif}` : `${styles.ongles}`}>
-                        <h2 className={styles.subtitle}>Beauté des pieds</h2>
-                        <ul>
-                            {beautyOfFoot.map(({ service, price }) => (
-                                <div className={styles.beautywrapper}>
-                                    <li>{service}</li>
-                                    <span className={styles.bold}>{price}</span>
-                                </div>
-                            ))}
-                        </ul>
-                    </div>
-                    <Image src={selectedService === 'Ongles' ? imgTopNails : selectedService === 'Cils' ? imgTopEyelashes : selectedService === 'Sourcils' ? imgTopEyebrows : ""} alt={""} classNamewrapper={styles.imgtopright} />
-                    <Image src={selectedService === 'Ongles' ? imgBottomNails : selectedService === 'Cils' ? imgBottomEyelashes : selectedService === 'Sourcils' ? imgBottomEyebrows : ""} alt={""} classNamewrapper={styles.imgbottomright} />
-                </div>
-            )}
-        </PageTransition>
-    )
+	return (
+		<PageTransition>
+			<Banner
+				src={img}
+				className={styles.img}
+				classNamewrapper={styles.wrapper}
+				title="Nos Services"
+				titleClass={styles.title}
+			/>
+			<ServiceBar
+				toggleService={selectedService}
+				changeClass={(selectedService: string) =>
+					setSelectedService(selectedService)
+				}
+			/>
+			{!selectedService ? (
+				<div className={styles.wrappercenter}>
+					<div className={styles.wrapperimgbottom}>
+						<img
+							src={defaultImgBottom}
+							alt=""
+							className={styles.img}
+						/>
+					</div>
+					<h1 className={styles.layoutitle}>
+						De la beauté du regard
+					</h1>
+					<div className={styles.wrapperimgtop}>
+						<img
+							src={defaultImgTop}
+							alt=""
+							className={styles.img}
+						/>
+					</div>
+				</div>
+			) : (
+				<div className={styles.wrappercenter}>
+					<div className={styles.price}>
+						<div
+							className={
+								selectedService === "Ongles"
+									? `${styles.ongles} ${styles.actif}`
+									: `${styles.ongles}`
+							}
+						>
+							<h2 className={styles.subtitle}>
+								Beauté des mains
+							</h2>
+							<ul>
+								{beautyOfHands.map(({ service, price }) => (
+									<div className={styles.beautywrapper}>
+										<li>{service}</li>
+										<span className={styles.bold}>
+											{price}
+										</span>
+									</div>
+								))}
+							</ul>
+							<div className={styles.test}>
+								<h2 className={styles.subtitle}>
+									Beauté des pieds
+								</h2>
+								<ul>
+									{beautyOfFoot.map(({ service, price }) => (
+										<div className={styles.beautywrapper}>
+											<li>{service}</li>
+											<span className={styles.bold}>
+												{price}
+											</span>
+										</div>
+									))}
+								</ul>
+							</div>
+						</div>
+						<div
+							className={
+								selectedService === "Sourcils"
+									? `${styles.sourcils} ${styles.actif}`
+									: `${styles.sourcils}`
+							}
+						>
+							<h2 className={styles.subtitle}>
+								Beauté du regard
+							</h2>
+							<ul>
+								{beautyOfEyebrows.map(({ service, price }) => (
+									<div className={styles.beautywrapper}>
+										<li>{service}</li>
+										<span className={styles.bold}>
+											{price}
+										</span>
+									</div>
+								))}
+							</ul>
+						</div>
+						<div
+							className={
+								selectedService === "Cils"
+									? `${styles.cils} ${styles.actif}`
+									: `${styles.cils}`
+							}
+						>
+							<h2 className={styles.subtitle}>
+								Beauté du regard
+							</h2>
+							<ul className={styles.listofservice}>
+								{beautyOfEyelashes.map(({ service, price }) => (
+									<div className={styles.beautywrapper}>
+										<li>{service}</li>
+										<span className={styles.bold}>
+											{price}
+										</span>
+									</div>
+								))}
+							</ul>
+						</div>
+					</div>
+					<div
+						className={
+							selectedService === "Ongles"
+								? `${styles.foot} ${styles.actif}`
+								: `${styles.ongles}`
+						}
+					>
+						<h2 className={styles.subtitle}>Beauté des pieds</h2>
+						<ul>
+							{beautyOfFoot.map(({ service, price }) => (
+								<div className={styles.beautywrapper}>
+									<li>{service}</li>
+									<span className={styles.bold}>{price}</span>
+								</div>
+							))}
+						</ul>
+					</div>
+					<Image
+						src={
+							selectedService === "Ongles"
+								? imgTopNails
+								: selectedService === "Cils"
+								? imgTopEyelashes
+								: selectedService === "Sourcils"
+								? imgTopEyebrows
+								: ""
+						}
+						alt={""}
+						classNamewrapper={styles.imgtopright}
+					/>
+					<Image
+						src={
+							selectedService === "Ongles"
+								? imgBottomNails
+								: selectedService === "Cils"
+								? imgBottomEyelashes
+								: selectedService === "Sourcils"
+								? imgBottomEyebrows
+								: ""
+						}
+						alt={""}
+						classNamewrapper={styles.imgbottomright}
+					/>
+				</div>
+			)}
+		</PageTransition>
+	);
 }
